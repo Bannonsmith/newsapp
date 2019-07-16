09c394127576ab236e048033c22559c32fce35a5
Bannonsmith <Bannonsmith13@gmail.com>
2019-07-15 23:47:56 -0500

    added post route and sync to database

--- app.js
+++ app.js
@@ -3,21 +3,36 @@ const express = require("express")
 const app = express()
 const mustacheExpress = require("mustache-express")
 const bodyParser = require("body-parser")
+const pgp = require("pg-promise")()
 const PORT = 3000
+const CONNECTION_STRING = "postgres://localhost:5432/newsdb"
 
 // Configuring your view engine
 app.engine("mustache",mustacheExpress())
 app.set("views","./views")
 app.set("view engine","mustache")
+
 app.use(bodyParser.urlencoded({extended: false}))
+
+const db = pgp(CONNECTION_STRING)
+
 
 app.post('/register', (req,res) => {
 
     let username = req.body.username
     let password = req.body.password
 
-    console.log(username,password)
-    res.send("register")
+    db.oneOrNone("SELECT userid  FROM users WHERE username = $1",[username])
+    .then((user) => {
+        if(user) {
+            res.render("register", {message: "User name already exists"})
+        } else {
+            db.none("INSERT INTO  users(username,password)  VALUES($1,$2)",[username,password])
+            .then(() => {
+                res.send("Success")
+            })
+        }
+    })
 })
 
 app.get('/register', (req,res)  => {
